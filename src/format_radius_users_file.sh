e02db59e506f01d2ab58b0941bca8460fedc0ba4
Julie Daligaud <furryjulie@fpsource.space>
2019-04-26 11:19:45 +0200

    Stop selectionning ver entry

--- src/format_radius_users_file.sh
+++ src/format_radius_users_file.sh
@@ -3,13 +3,14 @@
 NOW=$(date -u +%s)
 TMP_EXPORT="/tmp/${NOW}_format_radius.txt"
 
-OUT1=$( sed "/Cleartext-Password/!d" ./users )
+OUT1=$( sed "/(Cleartext-Password)|(Framed-IP-Address)/!d" ./users )
 echo "${OUT1}" > $TMP_EXPORT
 OUT2=$( sed  "s/Cleartext-Password :=//g" $TMP_EXPORT )
 echo "${OUT2}" > $TMP_EXPORT
 OUT3=$( sed  "s/\"//g" $TMP_EXPORT )
 echo "${OUT3}" > $TMP_EXPORT
-OUT4=$( sed "/ver/!d" $TMP_EXPORT)
+OUT4=$( sed  "s/Framed-IP-Address :=//g" $TMP_EXPORT )
+#OUT4=$( sed "/ver/!d" $TMP_EXPORT)
 echo "${OUT4}" > ./users_formated
 
 rm -f $TMP_EXPORT
